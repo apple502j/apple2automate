154dfb517b6d3108c88e508360e6438da523c23f
apple502j <33279053+apple502j@users.noreply.github.com>
2018-04-05 19:06:39 +0900

    Create words.py

---
+++ words.py
@@ -0,0 +1,76 @@
+#This includes many words.
+
+from random import randint
+
+WORDS_DIC_SUBJ_THIRDA = ['Apple','Ken','Banana','Jvvg','Everybody','Kaj','ST','She','He','The dog','Scratch Cat','Pico','Nano','Giga','Tera','Gobo','TemplatesFTW']
+WORDS_FIRST = "You"
+WORDS_SECOND="I"
+WORDS_DIC_SUBJ_THIRDM = ['They','The dogs', 'The cats','My bags','The bots','Scratchers','People']
+
+WORDS_VERB = [
+    {"name":"begin","third":"begins","past":"begun"},
+    {"name":"break","third":"breaks","past":"broke"},
+    {"name":"have","third":"has","past":"had"},
+    {"name":"want","third":"wants","past":"wanted"},
+    {"name":"need","third":"needs","past":"needed"},
+    {"name":"buy","third":"buys","past":"bought"},
+    {"name":"read","third":"reads","past":"read"},
+    {"name":"put","third":"puts","past":"put"},
+    {"name":"sing","third":"sings","past":"sung"},
+    {"name":"write","third":"writes","past":"wrote"},
+    {"name":"know","third":"knows","past":"knew"},
+    {"name":"use","third":"uses","past":"used"},
+    {"name":"make","third":"makes","past":"made"},
+    {"name":"cook","third":"cooks","past":"cooked"},
+]
+
+WORDS_NOUN = [
+    "a computer",
+    "a book",
+    "a clock",
+    "a cup",
+    "an eraser",
+    "a pen",
+    "a pencil",
+    "a notebook",
+    "a camera",
+    "a cell phone",
+    "an apple",
+    "an orange",
+    "a song",
+    "a dictionary",
+    "a card",
+    "a ruler",
+    "a bot",
+    "a man",
+    "a woman",
+    "a student",
+    "a teacher",
+    "a Scratcher",
+    "a textbook",
+    "a TV",
+    "a server",
+    "a fish",
+    "a head of lettuce",
+    "a bun",
+    "a ticket",
+    "a lily",
+    "a pancake",
+    "an egg",
+    "a carrot",
+    "an eggplant",
+    "a semicolon",
+    "a faucet",
+    "a bookstore",
+    "a shop",
+    "a store",
+    "a bakery",
+]
+
+WORDS_HOW = ["well","very well","quickly","loudly","abroad","alone","later"]
+
+WORDS_WHEN_NOW = ["every Wednesday","every Tuesday","every Monday","every Saturday","every Sunday","every Thursday","every Friday","every day","every weekend","every summer","every winter"]
+
+WORDS_WHEN_PAST = ["then","yesterday","last night","last Wednesday","last Tuesday","last Monday","last Saturday","last Sunday","last Thursday","last Friday",]
+
+def generate():
